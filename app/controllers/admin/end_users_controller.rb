ff64d534fd8b49ad7250b59cade08be17ef61d01
taniguchikouichi <kou.gtrnt0610@icloud.com>
2019-10-15 07:51:38 +0000

    add_admin_end_user

--- app/controllers/admin/end_users_controller.rb
+++ app/controllers/admin/end_users_controller.rb
@@ -5,14 +5,31 @@ def index
 end 
 def show
     @enduser = EndUser.find(params[:id])
+    @end_user = EndUser.new
 end
 def edit
+    @enduser = EndUser.find(params[:id])
+    @end_user = EndUser.new
 end
 def out
+    @enduser = EndUser.find(params[:id])
 end
 def update
+    @enduser = EndUser.find(params[:id])
+    if @enduser.update(@enduser_params)
+    redirect_to admin_end_users_path(@enduers.id)
+    else 
+    redirect_to edit_admin_end_users_path(@endusers)
+    end
 end
-def delete
+def destroy
+    @enduser = EndUser.find(params[:id])
+    @enduser.destroy
+    redirect_to admin_end_users_path
 end
 
+private
+def end_user_params
+   params.require(:end_user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :main_postal_code, :main_address, :phone_number, :email)
+end
 end
