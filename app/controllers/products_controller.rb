b3d20cbb30833a0f6fb75f7f524a236f37a82fff
taniguchikouichi <kou.gtrnt0610@icloud.com>
2019-10-19 06:39:21 +0000

    product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,9 @@
 class ProductsController < ApplicationController
     before_action :authenticate_end_user!
+    PER = 4
 def index
+    @product = Product.new
+    @products = Product.page(params[:page]).per(PER)
 end 
 def show
     @product = Product.find(params[:id])
