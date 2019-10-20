306006782f2219d7265608af43b6524721faa05a
湯浅なつき <yuasanatsuki@yuasanatsuki-no-MacBook-Air.local>
2019-10-20 14:27:59 +0900

    add_admin_orders_index

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,5 +1,16 @@
 class Admin::OrdersController < Admin::Base
 def index
+    @orders = Order.all.page(params[:page]).per(1)
+
+    @delivery_status = params[:delivery_status].to_i
+    case @delivery_status
+    when 0
+        @delivery_status = '受付'
+    when 1
+        @delivery_status = '商品準備中'
+    when 2
+        @delivery_status = '出荷済'
+    end
 end
 def show
 end
